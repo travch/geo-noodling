9e73dd3bc4d4276acac2fc1a0fd45f38511c770a
Travis Christ <travis.christ@auctane.com>
2025-07-18 14:46:10 -0500

    move map controls onto map, add yosemite boundaries

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
+import { GeoJSON, MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import { Camps } from './camps/Camps';
@@ -8,6 +8,8 @@ import { Tracks } from './tracks/Tracks';
 import { MapControls } from './map-controls/MapControls';
 
 import style from './MyMap.module.css';
+
+import yosemiteData from '../data/regions/yosemite.json';
 
 const LocationMarker = () => {
   useMapEvents({
@@ -21,15 +23,10 @@ const LocationMarker = () => {
 export const MyMap = () => {
   const [showCamps, setShowCamps] = useState(true);
   const [showTracks, setShowTracks] = useState(true);
+  const [showYosemite, setShowYosemite] = useState(true);
   return (
     <div className={style.wrapper}>
       <h1>Our John Muir Trail Experience</h1>
-      <MapControls
-        showCamps={showCamps}
-        setShowCamps={setShowCamps}
-        showTracks={showTracks}
-        setShowTracks={setShowTracks}
-      />
       <MapContainer
         className={style.map}
         center={[37.33079, -118.81027]}
@@ -39,7 +36,16 @@ export const MyMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
+        <MapControls
+          showCamps={showCamps}
+          setShowCamps={setShowCamps}
+          showTracks={showTracks}
+          setShowTracks={setShowTracks}
+          showYosemite={showYosemite}
+          setShowYosemite={setShowYosemite}
+        />
         <LocationMarker />
+        {showYosemite && <GeoJSON data={yosemiteData} />}
         {showCamps && <Camps />}
         {showTracks && <Tracks />}        
       </MapContainer>
--- src/components/map-controls/MapControls.jsx
+++ src/components/map-controls/MapControls.jsx
@@ -1,31 +1,38 @@
 import {
   Button,
+  Center,
   Checkbox,
   SimpleGrid,
   Popover,
 } from '@mantine/core';
+
+const togglerStyles = { zIndex: 401, position: 'absolute', left: '10px', top: '90px' };
+const popoverStyles = { zIndex: 402 };
 
 export const MapControls = ({
   showCamps,
   setShowCamps,
   showTracks,
   setShowTracks,
+  showYosemite,
+  setShowYosemite,
 }) => {
   return (
     <Popover
       width={200}
-      position='right'
+      position='left'
       withArrow
       shadow='md'
       trapFocus
     >
       <Popover.Target>
-        <Button>Map Controls</Button>
+        <Button style={togglerStyles}>Map Controls</Button>
       </Popover.Target>
-      <Popover.Dropdown style={{ zIndex: 401 }}>
+      <Popover.Dropdown style={popoverStyles}>
         <SimpleGrid
           spacing='lg'
         >
+          <Center>Map Elements</Center>
           <Checkbox
             checked={showCamps}
             label='Show Campsites'
@@ -35,6 +42,12 @@ export const MapControls = ({
             checked={showTracks}
             label='Show Route'
             onChange={() => setShowTracks(_curr => !_curr)}
+          />
+          <Center>Boundaries</Center>
+          <Checkbox
+            checked={showYosemite}
+            label='Yosemite'
+            onChange={() => setShowYosemite(_curr => !_curr)}
           />
         </SimpleGrid>
       </Popover.Dropdown>
